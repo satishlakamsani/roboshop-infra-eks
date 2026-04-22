803f36387e131d938a87001bb7f6e7b63b0a59da
satishlakamsani <lakamsanisatish@gmail.com>
2026-04-22 11:54:32 +0530

    added sg

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,6 +1,6 @@
 module "sg" {
     count = length(var.sg_names)
-    source = "../../terraform-aws-sg"
+    source = "../../terraform_aws_sg"
     project = var.project
     environment = var.environment
     sg_name = replace(var.sg_names[count.index], "_", "-")
