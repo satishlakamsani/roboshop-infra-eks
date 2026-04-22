9874272bead705a9b78f24152c28e59ea1c64d44
satishlakamsani <lakamsanisatish@gmail.com>
2026-04-22 13:10:48 +0530

    added sg

--- 80-frontend-alb/locals.tf
+++ 80-frontend-alb/locals.tf
@@ -1,7 +1,8 @@
 locals {
-  frontend_alb_sg_id = data.aws_ssm_parameter.ingress_alb_sg_id.value
+  ingress_alb_sg_id = data.aws_ssm_parameter.ingress_alb_sg_id.value
   public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
   frontend_alb_certificate_arn = data.aws_ssm_parameter.frontend_alb_certificate_arn.value
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = {
         Project = var.project
         Environment = var.environment
